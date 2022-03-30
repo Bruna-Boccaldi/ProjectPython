5a4aee4890a899424c3d676fbfa498a99e445862
Bruna-Boccaldi <boccaldibruna@gmail.com>
2022-03-30 01:13:10 -0300

    Commit 30/03

--- Application/Novo.java
+++ Application/Novo.java
@@ -3,8 +3,11 @@ package Application;
 public class Novo{
 
     public static void main(String[] args){
+        String color = "\u001B[";
 
-        System.out.println("Hello Word em Java");
+        System.out.println();
+        System.out.println(color+"32"+"m"+"Hello Word em Java"+color+"m");
+        System.out.println();
 
     }
 }
